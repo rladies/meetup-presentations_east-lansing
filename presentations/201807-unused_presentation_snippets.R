07be8246b6e4d79f2186d11f8015945e1b7fa242
jananiravi <janani@msu.edu>
2018-07-23 21:01:44 -0400

    kickoff meeting presentation + reorganization
    - using ioslides, first
    - then moved to Xaringan | @yihui
    - Alison Hill theme @apreshill
    - reorganized other folders/images/content

---
+++ presentations/201807-unused_presentation_snippets.R
@@ -0,0 +1,39 @@
+---
+title: "Kick-off Meeting"
+author: "Janani Ravi | jananiravi@rladies.org"
+date: July 25, 2018
+output:
+  ioslides_presentation:
+    css: shiny-slides.css
+    logo: ../logos-qrcode/RLadiesEastLansing-sqbig.png
+    self_contained: no
+    incremental: true
+    widescreen: true
+---
+
+# Inserting an image
+```{r, out.width = "600px", echo=FALSE}
+knitr::include_graphics("../logos-qrcode/rshinylady-stats-map.png")
+```
+
+# Unused xaringan
+```
+output:
+	xaringan::moon_reader:
+	css: ["default", "rladies", "rladies-fonts"]
+logo: ../logos-qrcode/RLadiesEastLansing-sqbig.png
+lib_dir: libs
+nature:
+	highlightStyle: github
+highlightLines: true
+countIncrementalSlides: false
+includes:
+	in_header: header.html
+```
+---
+# using background image
+```
+background-image: url("../logos-qrcode/rshinylady-stats-map.png")
+background-position: 0% 0%
+```
+---
---
+++ sponsors-contacts/contacts-nonMSU.md
@@ -0,0 +1,32 @@
+## Twitter
+- [ ] WomenInEngg
+- [ ] WomenInStem
+- [ ] GirlsWhoCode
+
+## High school
+- [ ] Contact from @archerc5
+- [ ] Contact from Dimity, CVM?
+
+## Ann Arbor
+### UMich
+- [x] MIDAS | WIDS | Moira Dowling
+- [x] Friends at UMich — Sethu, Visa, Sriram
+
+## Grand Rapids
+## MSU College of Human Medicine
+- [ ] ??
+
+## Van Andel Research Institute
+- [ ] Megan Bowman
+- [ ] Ben Johnson
+
+## Big Data Ignite
+- [x] Nichole Nadkarni
+- [x] Rivka
+
+## Lansing RUG
+- [x] Emilio Esposito
+- [x] Joe Kelly
+
+## MHA
+- [x] Renee Smiddy, Joe Kelly
