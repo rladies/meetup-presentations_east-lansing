4cccee585c1a956aaf57e71534eafdbcbdfb6909
jananiravi <janani@msu.edu>
2018-10-26 15:46:08 -0400

    preparing for 20181105 tidy data workshop

---
+++ presentations/20181105-workshop-tidydata/20181105-intro-to-tidydata.R
@@ -0,0 +1,8 @@
+## Part 0: Installation and set-up
+## Install RStudio from: http://rstudio.com/download
+## Install tidyverse using:
+install.packages("tidyverse")
+## Load tidyverse
+library(tidyverse)
+
+## Part 1: dplyr
