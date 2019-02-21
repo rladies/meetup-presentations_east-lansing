7abfac08d2aa1f483bf1a2ba4ec1a3f99a3a94aa
jananiravi <janani@msu.edu>
2019-02-21 14:08:55 -0500

    Create rstats-workshop3.R

---
+++ presentations/20190318-workshop-rstats/rstats-workshop3.R
@@ -0,0 +1,18 @@
+# Ch 11: Begin with Hypothesis testing (Navarro Ch 11)?
+# Or mention p-values, effect sizes, etc. within each test? (15min)
+
+# Corrected p-values - Janani’s code :-) (p.adjust(): Bonferroni vs BH)
+
+# FDR
+
+# Ch 11.8: Review Power tests -- show R code lines, briefly mention why needed (but if people have specific questions for their study, they may have to figure that out for themselves) (15min)
+
+# Exercise w/ Gapminder (15min)
+
+# Parametric (t) & Non-parametric statistical tests (Wilcox) (20min)
+
+# Ch 12: chi-square
+
+# Ch 13: t-test
+
+# Ch 13: Wilcoxon
