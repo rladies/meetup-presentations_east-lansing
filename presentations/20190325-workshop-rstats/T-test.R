b481e05df628492ceb9588c34c3b884c49f18743
jananiravi <janani@msu.edu>
2019-03-25 14:30:19 -0400

    201903 RStats Workshop

--- presentations/20190325-workshop-rstats/T-test.R
+++ presentations/20190325-workshop-rstats/T-test.R
@@ -3,47 +3,53 @@
 # Data from W.S. Gosset's 1908 paper on the t-test.
 # Two different sleeping drugs were taken by two groups of patients.
 
-# The variable "extra" is the increase in hours of sleep on the groups 
+# The variable "extra" is the increase in hours of sleep on the groups
 # (consisting of 10 patients each).
 # The variable "group" gives the labels for which drug each patient took.
 
 # The sleeping data is a built-in data set in R,
 
 ## Installing packages
-install.packages("ggpubr")
-install.packages("lsr")
-install.packages("ggplot2")
-install.packages("pwr")
+install.packages("ggpubr") # publicationr ready plots w/ ggplot2
+install.packages("lsr") # Learning Statistics w/ R by Dani Navarro
+# install.packages("tidyverse")
+# install.packages("ggplot2") # if you don't have tidyverse installed
+install.packages("car")
+install.packages("pwr") # basic functions of power analysis
 
 # Loading packages
-library(ggplot2)
+library(ggpubr)
+library(tidyverse) # loads ggplot2, tidyr, dplyr, ...
 library(lsr)
 library(car)
 library(pwr)
 
-# Load and attach the sleep dataframe
-data(sleep)
-attach (sleep)
+# There's no need to explicitly load and attach the sleep dataframe
+# 'datasets' package is pre-loaded. So is 'sleep'.
+# data(sleep)
+# attach(sleep)
 
 #Viewing the data:
 View(sleep)
+glimpse(sleep)
 
 # Do the spreads seem equal across groups? -- We'll address verifying our intuition later.
-ggplot(sleep, aes(x=group, y=extra)) + 
+ggplot(sleep, aes(x=group, y=extra)) +
   geom_boxplot() +
   theme_minimal()
 
 
 # Do the data appear normally distributed?
 ggplot(sleep, aes(x=extra, color=group, fill=group)) +
-  geom_histogram(aes(y=..density..), binwidth = 0.5, position="identity", alpha=0.2)+   # Histogram with density instead of count on y-axis
-  geom_density(alpha=0.6)+
+  geom_histogram(aes(y=..density..), binwidth = 0.5,
+                 position="identity", alpha=0.2) +   # Histogram with density instead of count on y-axis
+  geom_density(alpha=0.6) + # adding a density plot
 #  geom_vline(data=mu, aes(xintercept=grp.mean, color=group),
  #            linetype="dashed")+
-  scale_color_manual(values=c("#999999", "#E69F00", "#56B4E9"))+
-  scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9"))+
-  labs(title="Sleep histogram plot",x="Extra Sleep (Hrs.)", y = "Density")+
-  theme_classic()
+  scale_color_manual(values=c("#999999", "#E69F00", "#56B4E9")) + # for outline
+  scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9")) +  # for fill
+  labs(title="Sleep histogram plot",x="Extra Sleep (Hrs.)", y = "Density") + # annotating the plot
+  theme_classic() # no grey background or grids
 
 ##T-tests
 # Assuming equal variances (Student's independent t-test):
@@ -57,39 +63,41 @@ pairedT = t.test(extra ~ group, data = sleep, paired = TRUE)
 
 
 # Extracting key information
-t-statistic = indpT$statistic
-p-val = indpT$p.value
-ci_95 = indpT$conf.int
-df = indpT$parameter
+# Tip: name your variables using underscores rather than hyphens; hyphens are considered to be 'minuses'
+t_statistic <- indpT$statistic
+p_val <- indpT$p.value
+ci_95 <- indpT$conf.int
+df <- indpT$parameter
 
 
-#Effect Size: How significant is significant?
+## Effect Size: How significant is significant?
 # Compute the Cohen's d or Hedges'g effect size statistics.
 # cohensD( x = NULL, y = NULL, data = NULL, method = c("pooled","raw", "corrected", "paired", "x.sd", "y.sd"), mu = 0, formula = NULL )
 library(lsr)
 effect_size = cohensD(extra ~ group, data = sleep)
 
-#Quantification of the effect size magnitude is performed using the thresholds define in Cohen (1992). 
+## Quantification of the effect size magnitude is performed using the thresholds define in Cohen (1992).
 #The magnitude is assessed using the thresholds provided in (Cohen 1992), i.e. |d|<0.2 "negligible", |d|<0.5 "small", |d|<0.8 "medium", otherwise "large"
 #More examples: https://rdrr.io/cran/lsr/man/cohensD.html
 #Hodge's g vs. cohen's d:  https://stats.stackexchange.com/questions/1850/difference-between-cohens-d-and-hedges-g-for-effect-size-metrics
 
-# Power Analysis
-#Power is the probability of rejecting the null hypothesis when the specific alternative hypothesis is true. 
-#library(pwr)
+## Power Analysis
+#Power is the probability of rejecting the null hypothesis when the specific alternative hypothesis is true.
+library(pwr)
 #pwr.t.test(n = length(sleep$extra)/2, d = , sig.level = 0.05 , power = 0.8, type = c("two.sample", "one.sample", "paired"))
 
 #calculate the necessary sample size for a specified power
 s.d= sd(sleep$extra)
 pwr.t.test(d = effect_size, sig.level = 0.05 , power = 0.8, type = "two.sample", alternative = "two.sided")
-# where n is the sample size, d is the effect size, and type indicates a two-sample t-test, one-sample t-test or paired t-test. 
+# where n is the sample size, d is the effect size, and type indicates a two-sample t-test, one-sample t-test or paired t-test.
 
 # calculate the power when given a specific sample size
 len = length(sleep$extra)/2
-pwr.t.test(n= len, d = effect_size, sig.level = 0.05 , type = "two.sample", alternative = "two.sided")
+pwr.t.test(n= len, d = effect_size, sig.level = 0.05,
+           type = "two.sample", alternative = "two.sided")
 
 #If you have unequal sample sizes, use the following, where n1 and n2 are the sample sizes
-pwr.t2n.test(n1 = , n2= , d = , sig.level =, power = )
+pwr.t2n.test(n1 = , n2= , d = , sig.level = , power = )
 
 
 
@@ -108,8 +116,8 @@ ggqqplot(sleep$extra)
 shapiro.test(sleep$extra)
 
 #Equal Variance
-#Levene’s test checks for homogeneity of variances and the null hypothesis is that all variances are equal. 
-#A resulting p-value under 0.05 means that variances are not equal and than further parametric tests such as ANOVA are not suited. 
+#Levene’s test checks for homogeneity of variances and the null hypothesis is that all variances are equal.
+#A resulting p-value under 0.05 means that variances are not equal and than further parametric tests such as ANOVA are not suited.
 #Note that this test is meant to be used with normally distributed data but can tolerate relatively low deviation from normality.
 
 
